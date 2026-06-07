a3dd873c75815511a6038c9deb5b15e5687d9cc9
Killua <darkegame101@gmail.com>
2026-06-07 10:05:19 +0700

    add blog ,fix ui voucher page

---
+++ refactor_blog2.js
@@ -0,0 +1,10 @@
+﻿const fs = require('fs');
+
+let content = fs.readFileSync('blog.html', 'utf8');
+
+// Replace everything inside post-list with empty div
+const listRegex = /<div class="post-list">[\s\S]*?<\/div>\s*<!-- Sidebar -->/;
+content = content.replace(listRegex, '<div class="post-list" id="blog-grid"></div>\n\n      <!-- Sidebar -->');
+
+fs.writeFileSync('blog.html', content, 'utf8');
+console.log('blog.html refactored post-list');
